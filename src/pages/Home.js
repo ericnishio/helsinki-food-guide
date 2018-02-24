e6e8bc2d86c8fa8496086169a66813ea36901b87
Eric Nishio <ericnishio@protonmail.com>
2018-02-24 20:31:13 +0200

    Animate list on ready

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import styled, {keyframes} from 'styled-components'
 
 import {Row, Content} from '../common/components/Grid'
 import Hero from '../common/components/Hero'
@@ -38,25 +39,47 @@ class Home extends Component {
     return (
       <Row>
         <Content>
-          <Hero>
-            <Heading>
-              Know exactly what to order.<br />
-              Featuring only the best dishes in Helsinki.
-            </Heading>
-          </Hero>
-          {
-            dishes.map(dish =>
-              <Dish
-                key={dish.name}
-                dish={dish}
-                restaurant={dish.restaurant}
-              />
-            )
-          }
+          <HeroContainer>
+            <Hero>
+              <Heading>
+                Know exactly what to order.<br />
+                Featuring only the best dishes in Helsinki.
+              </Heading>
+            </Hero>
+          </HeroContainer>
+          <DishContainer>
+            {
+              dishes.map(dish =>
+                <Dish
+                  key={dish.name}
+                  dish={dish}
+                  restaurant={dish.restaurant}
+                />
+              )
+            }
+          </DishContainer>
         </Content>
       </Row>
     )
   }
 }
+
+const fadeIn = keyframes`
+  0% {
+    opacity: 0;
+  }
+
+  100% {
+    opacity: 1;
+  }
+`
+
+const HeroContainer = styled.div`
+  animation: ${fadeIn} 0.8s;
+`
+
+const DishContainer = styled.div`
+  animation: ${fadeIn} 0.8s;
+`
 
 export default Home
