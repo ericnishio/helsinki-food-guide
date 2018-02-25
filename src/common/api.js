a0fcf8640c01a61ef74078e2435e8f20a9db7d7e
Eric Nishio <ericnishio@protonmail.com>
2018-02-25 22:16:40 +0200

    Simplify code

--- src/common/api.js
+++ src/common/api.js
@@ -5,22 +5,13 @@ import {setLocalStorageValue, getLocalStorageValue} from './localStorage'
 
 export const ABOUT_ENTRY_ID = '2Uqcv9yuQEIWoQu8SW4kAG'
 
-const state = {
-  isOnline: navigator.onLine,
-}
-
-window.addEventListener('load', () => {
-  window.addEventListener('online', () => state.isOnline = navigator.onLine)
-  window.addEventListener('offline', () => state.isOnline = navigator.onLine)
-})
-
 const client = createClient({
   space: '9jmm61ik3148',
   accessToken: 'c2a2be0d37960b9f71ba0e0d31ac38bb908dc4a226bb311802d0361e4bcb700f',
 })
 
 export const loadDishes = async () => {
-  if (!state.isOnline) {
+  if (!navigator.onLine) {
     return getLocalStorageValue('dishes') || []
   }
 
@@ -57,4 +48,3 @@ const loadEntryById = async (id) => {
 
 export const loadAboutPage = () =>
   loadEntryById(ABOUT_ENTRY_ID)
-
