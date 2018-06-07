133aaa6f282bfc9b7323eb785eceb4673d7618f8
Eric Nishio <ericnishio@protonmail.com>
2018-06-07 11:02:47 +0300

    Remove certbot scripts

--- scripts/certbot.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-certbot certonly \
---manual \
---manual-auth-hook $DIR/certbot-auth.sh \
---manual-cleanup-hook $DIR/certbot-cleanup.sh \
--d helsinkifoodguide.com -d www.helsinkifoodguide.com
