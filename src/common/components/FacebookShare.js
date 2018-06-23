65c6dc5464fdf85df5bc758cd4e4c04a243b82b9
Eric Nishio <ericnishio@protonmail.com>
2018-06-24 00:53:28 +0300

    Ensure Facebook buttons appear on page

--- src/common/components/FacebookShare.js
+++ src/common/components/FacebookShare.js
@@ -1,27 +1,43 @@
-import React from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 
-const FacebookShare = ({size = 'small', style}) =>
-  <div
-    className="fb-share-button"
-    data-href="https://helsinkifoodguide.com"
-    data-layout="button_count"
-    data-size={size}
-    data-mobile-iframe="true"
-    style={style}
-  >
-    <a
-      target="_blank" // eslint-disable-line
-      href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fhelsinkifoodguide.com%2F&amp;src=sdkpreparse"
-      className="fb-xfbml-parse-ignore"
-    >
-      Share
-    </a>
-  </div>
+class FacebookShare extends Component {
+  static propTypes = {
+    size: PropTypes.string,
+    style: PropTypes.object,
+  }
 
-FacebookShare.propTypes = {
-  size: PropTypes.string,
-  style: PropTypes.object,
+  static defaultProps = {
+    size: 'small',
+  }
+
+  componentDidMount() {
+    // eslint-disable-next-line
+    if (typeof FB !== 'undefined') FB.XFBML.parse() // ensure Facebook buttons appear on page
+  }
+
+  render() {
+    const {size, style} = this.props
+
+    return (
+      <div
+        className="fb-share-button"
+        data-href="https://helsinkifoodguide.com"
+        data-layout="button_count"
+        data-size={size}
+        data-mobile-iframe="true"
+        style={style}
+      >
+        <a
+          target="_blank" // eslint-disable-line
+          href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fhelsinkifoodguide.com%2F&amp;src=sdkpreparse"
+          className="fb-xfbml-parse-ignore"
+        >
+          Share
+        </a>
+      </div>
+    )
+  }
 }
 
 export default FacebookShare
