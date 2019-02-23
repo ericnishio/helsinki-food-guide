88da70f4f80b3fad40642795657ba3c9e99629ad
Eric Nishio <ericnishio@protonmail.com>
2019-02-23 10:31:43 +0200

    Updates

---
+++ precache-manifest.cb8b6346ee827c892d70c8de2f127a03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "812e83c3714c61860054",
+    "url": "/helsinki-food-guide/static/js/main.812e83c3.chunk.js"
+  },
+  {
+    "revision": "78b88906d9228edbfd16",
+    "url": "/helsinki-food-guide/static/js/runtime~main.78b88906.js"
+  },
+  {
+    "revision": "02e386d46b50affcb838",
+    "url": "/helsinki-food-guide/static/js/2.02e386d4.chunk.js"
+  },
+  {
+    "revision": "b2562482416f1bdaa812be5ebf6254f3",
+    "url": "/helsinki-food-guide/static/media/logo.b2562482.png"
+  },
+  {
+    "revision": "3d74e8bfd4ef7985f7529bb9f7650eca",
+    "url": "/helsinki-food-guide/static/media/twitter.3d74e8bf.png"
+  },
+  {
+    "revision": "779840afa7ebc4edfc8a74d4a9758341",
+    "url": "/helsinki-food-guide/static/media/broken-egg.779840af.png"
+  },
+  {
+    "revision": "1e9ab879e63511eb601f829d4cef168f",
+    "url": "/helsinki-food-guide/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/helsinki-food-guide/precache-manifest.cb8b6346ee827c892d70c8de2f127a03.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/helsinki-food-guide/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
